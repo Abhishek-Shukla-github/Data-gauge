3add7fa433da07b1cca0a983faeaf79ec9a4e2f0
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2023-05-23 12:38:34 +0530

    Theme setup for light and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 // import { useState } from 'react'
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
-
+  const [theme, colorMode] = useMode();
   return (
-    <div className='app'>
-      React Data Gauge
-    </div>
+  <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <main className="content"></main>
+        </div>
+        </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
