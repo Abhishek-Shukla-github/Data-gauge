ebd9cbd66507ff740d0814554390afb72de66bd3
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2023-05-23 13:52:24 +0530

    Add Topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,7 +10,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
-          <main className="content"></main>
+          <main className="content">
+            <Topbar />
+          </main>
         </div>
         </ThemeProvider>
     </ColorModeContext.Provider>
